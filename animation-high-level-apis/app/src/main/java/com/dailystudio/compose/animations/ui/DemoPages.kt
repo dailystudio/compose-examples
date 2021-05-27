0a26535dd1826837c54a4eb188fe78b5402a8a6d
Nan YE <dailystudio2010@gmail.com>
2021-05-28 00:06:25 +0800

    [animation-high-level][DEV]: adjust layouts and display content;

--- animation-high-level-apis/app/src/main/java/com/dailystudio/compose/animations/ui/DemoPages.kt
+++ animation-high-level-apis/app/src/main/java/com/dailystudio/compose/animations/ui/DemoPages.kt
@@ -22,26 +22,65 @@ const val MIN_IMAGE_HEIGHT = 200
 const val MAX_IMAGE_HEIGHT = 400
 const val IMAGE_HEIGHT_DELTA = 50
 
+const val COLLAPSE_ARTICLE_HEIGHT = 85
+const val EXPAND_ARTICLE_HEIGHT = 200
+
 @Composable
 fun DemoPageA() {
     Column(modifier = Modifier
         .fillMaxSize(),
-        verticalArrangement = Arrangement.Center) {
+        verticalArrangement = Arrangement.Center
+    ) {
         Text(
             text = stringResource(id = R.string.label_demo_page_a),
+            style = MaterialTheme.typography.h6,
+            color = MaterialTheme.colors.primary,
+
             modifier = Modifier
-                .fillMaxWidth(),
+                .fillMaxWidth()
+                .padding(8.dp),
             textAlign = TextAlign.Center,
         )
+
+        var articleHeight by remember {
+            mutableStateOf( COLLAPSE_ARTICLE_HEIGHT )
+        }
+
+        Card(modifier = Modifier
+            .padding(horizontal = 16.dp),
+            elevation = 16.dp
+        ) {
+            Text(
+                text = stringResource(id = R.string.article),
+                style = MaterialTheme.typography.body1,
+                modifier = Modifier
+                    .height(articleHeight.dp + 16.dp)
+                    .clickable {
+                        articleHeight = if (articleHeight == COLLAPSE_ARTICLE_HEIGHT) {
+                            EXPAND_ARTICLE_HEIGHT
+                        } else {
+                            COLLAPSE_ARTICLE_HEIGHT
+                        }
+                    }
+                    .padding(8.dp)
+                    .animateContentSize()
+                    .fillMaxWidth(),
+                textAlign = TextAlign.Center,
+            )
+        }
     }
 }
 
 @Composable
 fun DemoPageB() {
     Column(modifier = Modifier
-        .fillMaxSize()) {
+        .fillMaxSize(),
+        verticalArrangement = Arrangement.Center
+    ) {
         Text(
             text = stringResource(id = R.string.label_demo_page_b),
+            style = MaterialTheme.typography.h6,
+            color = MaterialTheme.colors.primary,
             modifier = Modifier
                 .fillMaxWidth()
                 .padding(8.dp)
@@ -50,12 +89,13 @@ fun DemoPageB() {
         )
 
         var imageHeight by remember {
-            mutableStateOf( MIN_IMAGE_HEIGHT)
+            mutableStateOf( MIN_IMAGE_HEIGHT )
         }
 
         Card(modifier = Modifier
-            .padding(horizontal = 16.dp)
             .animateContentSize()
+            .padding(horizontal = 16.dp)
+            .height(imageHeight.dp)
             .clickable {
                 imageHeight += IMAGE_HEIGHT_DELTA
                 imageHeight = imageHeight.coerceIn(MIN_IMAGE_HEIGHT, MAX_IMAGE_HEIGHT)
@@ -67,7 +107,6 @@ fun DemoPageB() {
                 contentDescription = null,
                 contentScale = ContentScale.Crop,
                 modifier = Modifier
-                    .height(imageHeight.dp)
             )
         }
     }
