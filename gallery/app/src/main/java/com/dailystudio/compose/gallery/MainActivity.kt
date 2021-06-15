9c0a9fbf47ba11ff66bee2585cbae6ba0a1e9b1f
Nan YE <dailystudio2010@gmail.com>
2021-06-15 16:55:44 +0800

    [gallery][DEV]: a basic layout for photos;

--- gallery/app/src/main/java/com/dailystudio/compose/gallery/MainActivity.kt
+++ gallery/app/src/main/java/com/dailystudio/compose/gallery/MainActivity.kt
@@ -3,36 +3,47 @@ package com.dailystudio.compose.gallery
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.paging.ExperimentalPagingApi
+import androidx.paging.Pager
+import androidx.paging.PagingConfig
+import androidx.paging.PagingData
+import androidx.paging.compose.collectAsLazyPagingItems
+import com.dailystudio.compose.gallery.api.UnsplashApiInterface
+import com.dailystudio.compose.gallery.db.PhotoItem
+import com.dailystudio.compose.gallery.db.PhotoItemMediator
+import com.dailystudio.compose.gallery.model.PhotoItemViewModel
+import com.dailystudio.compose.gallery.ui.Photos
 import com.dailystudio.compose.gallery.ui.theme.GalleryTheme
+import com.dailystudio.devbricksx.development.Logger
+import java.util.concurrent.Flow
 
 class MainActivity : ComponentActivity() {
+    @ExperimentalPagingApi
+    @ExperimentalFoundationApi
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
+            val viewModel = viewModel<PhotoItemViewModel>()
+            val photos = Pager(
+                PagingConfig(pageSize = UnsplashApiInterface.DEFAULT_PER_PAGE),
+                remoteMediator = PhotoItemMediator()
+            ) {
+                viewModel.listPhotos()
+            }.flow.collectAsLazyPagingItems()
+
             GalleryTheme {
-                // A surface container using the 'background' color from the theme
                 Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
+                    Photos(photos)
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    GalleryTheme {
-        Greeting("Android")
     }
 }
---
+++ gallery/app/src/main/java/com/dailystudio/compose/gallery/ui/Photos.kt
@@ -0,0 +1,72 @@
+package com.dailystudio.compose.gallery.ui
+
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.GridCells
+import androidx.compose.foundation.lazy.LazyVerticalGrid
+import androidx.compose.foundation.shape.CornerSize
+import androidx.compose.material.Card
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.unit.dp
+import androidx.paging.compose.LazyPagingItems
+import coil.size.Scale
+import com.dailystudio.compose.gallery.db.PhotoItem
+import com.google.accompanist.coil.rememberCoilPainter
+
+@ExperimentalFoundationApi
+@Composable
+fun Photos(photoItems: LazyPagingItems<PhotoItem>) {
+
+    LazyVerticalGrid(
+        cells = GridCells.Fixed(2)
+    ) {
+        items(photoItems.itemCount) { index ->
+            val p = photoItems.getAsState(index = index).value ?: return@items
+
+            Card(modifier = Modifier
+                .height(250.dp)
+                .padding(8.dp),
+                elevation = 3.dp,
+                shape = MaterialTheme.shapes.medium.copy(
+                    CornerSize(5.dp)
+                )
+            ) {
+                Surface(color = Color.Black) {
+                    Box(modifier = Modifier.fillMaxSize(),
+                        contentAlignment = Alignment.BottomEnd
+                    ) {
+                        Image(
+                            painter = rememberCoilPainter(p.thumbnailUrl),
+                            contentScale = ContentScale.Crop,
+                            contentDescription = null,
+                        )
+                        Text(
+                            text = "by ${p.author}",
+                            style = MaterialTheme.typography.body2.copy(
+                                color = Color.White
+                            ),
+                            overflow = TextOverflow.Ellipsis,
+                            maxLines = 1,
+                            modifier = Modifier.padding(5.dp)
+                        )
+                    }
+                }
+            }
+        }
+        
+    }
+}
