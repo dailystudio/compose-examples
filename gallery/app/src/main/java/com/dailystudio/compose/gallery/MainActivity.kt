5dc06e3d1729d1e7941fefbeeb762e75df3a77b3
Nan YE <dailystudio2010@gmail.com>
2021-06-16 16:29:18 +0800

    [gallery][DEV]: new ic_launcher; [gallery][DEV]: add about;

--- gallery/app/src/main/java/com/dailystudio/compose/gallery/MainActivity.kt
+++ gallery/app/src/main/java/com/dailystudio/compose/gallery/MainActivity.kt
@@ -4,11 +4,19 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.widthIn
 import androidx.compose.material.*
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.collectAsState
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Info
+import androidx.compose.material.icons.filled.MoreVert
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.paging.ExperimentalPagingApi
 import androidx.paging.Pager
@@ -20,41 +28,85 @@ import com.dailystudio.compose.gallery.api.UnsplashApiInterface
 import com.dailystudio.compose.gallery.db.PhotoItem
 import com.dailystudio.compose.gallery.db.PhotoItemMediator
 import com.dailystudio.compose.gallery.model.PhotoItemViewModel
+import com.dailystudio.compose.gallery.ui.AboutDialog
 import com.dailystudio.compose.gallery.ui.Photos
 import com.dailystudio.compose.gallery.ui.theme.GalleryTheme
 import com.dailystudio.devbricksx.development.Logger
 import java.util.concurrent.Flow
 
 class MainActivity : ComponentActivity() {
-    @ExperimentalPagingApi
     @ExperimentalFoundationApi
+    @ExperimentalPagingApi
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            val viewModel = viewModel<PhotoItemViewModel>()
-            val photos = Pager(
-                PagingConfig(pageSize = UnsplashApiInterface.DEFAULT_PER_PAGE),
-                remoteMediator = PhotoItemMediator()
-            ) {
-                viewModel.listPhotos()
-            }.flow.collectAsLazyPagingItems()
-
             GalleryTheme {
-                Home(photos)
+                Home()
             }
         }
     }
 }
 
 
+@ExperimentalPagingApi
 @ExperimentalFoundationApi
 @Composable
-fun Home(photos: LazyPagingItems<PhotoItem>) {
+fun Home() {
+    val viewModel = viewModel<PhotoItemViewModel>()
+    val photos = Pager(
+        PagingConfig(pageSize = UnsplashApiInterface.DEFAULT_PER_PAGE),
+        remoteMediator = PhotoItemMediator()
+    ) {
+        viewModel.listPhotos()
+    }.flow.collectAsLazyPagingItems()
+
+    var showMenu by remember { mutableStateOf(false) }
+
+    var showAboutDialog by remember {
+        mutableStateOf(false)
+    }
+
     Scaffold(topBar = {
-        TopAppBar(title = {
-            Text(text = stringResource(id = R.string.app_name))
-        })
+        TopAppBar(
+            title = {
+                Text(text = stringResource(id = R.string.app_name))
+            },
+            actions = {
+                IconButton(onClick = {
+                    showMenu = true
+                }) {
+                    Icon(Icons.Default.MoreVert, "More actions")
+                }
+                DropdownMenu(
+                    expanded = showMenu,
+                    onDismissRequest = { showMenu = false }
+                ) {
+                    DropdownMenuItem(onClick = {
+                        showAboutDialog = true
+                        showMenu = false
+                    }) {
+                        Row(
+                            verticalAlignment = Alignment.CenterVertically,
+                            modifier = Modifier.widthIn(min = 100.dp)
+                        ) {
+                            Icon(
+                                Icons.Filled.Info,
+                                contentDescription = null,
+                                modifier = Modifier.padding(8.dp)
+                            )
+                            Text(stringResource(id = R.string.menu_about))
+                        }
+                    }
+                }
+            }
+
+        )
     }) {
         Photos(photos)
+
+        AboutDialog(showDialog = showAboutDialog) {
+            showAboutDialog = false
+        }
+
     }
 }
